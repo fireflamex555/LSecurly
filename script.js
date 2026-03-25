bb2ce098053701871d07ada00347acc3e02b786d
fireflamex555 <rayansharma02@gmail.com>
2026-03-24 20:50:20 -0700

    Add event listener to show game container

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 // script.js
 console.log("Script loaded successfully!");
+document.getElementById("playBtn").addEventListener("click", () => {
+    document.getElementById("gameContainer").style.display = "block";
+});
